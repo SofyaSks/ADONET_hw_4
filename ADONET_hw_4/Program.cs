a724526bb6ba825b4bcb62df9b7a93572c0a6b0e
sks <ssurovneva@mail.ru>
2024-02-19 21:55:34 +0300

    Add project files.

---
+++ ADONET_hw_4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONET_hw_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DataClasses1DataContext db = new DataClasses1DataContext())
+            {
+
+                DateTime today = DateTime.Now;
+                DateTime monthFresh = today.AddMonths(-1);
+
+                Console.WriteLine("Сортировка по дате");
+                IReadOnlyList<song> songs = (from song in db.songs
+                                             orderby song.date descending
+                                             select song).ToList();
+
+                foreach (var item in songs)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+                Console.WriteLine("\nСортировка по добавлению (месяц назад или позже)");
+                IReadOnlyList<song> songs2 = (from song in db.songs
+                                              where ((song.date.Year == today.Year) && (song.date.Month > monthFresh.Month))                                            
+                                              orderby song.title
+                                              select song).ToList();
+
+                foreach (var item in songs2)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("\nСамая новая песня");
+
+
+                var newestSong = (from song in db.songs
+                                  orderby song.date descending
+                                  select song).First();
+
+                Console.WriteLine(newestSong); 
+                
+                Console.WriteLine("\nСамая старая песня");
+
+                var oldetSong = (from song in db.songs
+                                  orderby song.date 
+                                  select song).First();
+
+                Console.WriteLine(oldetSong);
+
+            }
+        }
+    }
+}
---
+++ ADONET_hw_4/song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONET_hw_4
+{
+    public partial class song
+    {
+        public override string ToString()
+        {
+            return $" {Id, -3} {title, -20} {rating,-20} {weight,-20} {duration,-20} {date.ToShortDateString(),-20}";
+        }
+    }
+}
